d92a1344565dd5ce1214c752ff40e81c567a067c
sherbakov andrei <asherbakov@nopreset.ru>
2025-08-28 13:47:04 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import {useEffect} from "react";
-import Button from "./components/Buttons/Button";
-import Form from "./components/Forms/Form";
-import Input from "./components/Inputs/Input";
 import {useTelegram} from "./Hooks/useTelegram";
-import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom';
 import Main from "./components/Pages/Main/Main";
 import Footer from "./components/Footer/Footer";
 import Favorite from "./components/Pages/Favorite/Favorite";
 import Add from "./components/Pages/Add/Add";
+
 export default function App() {
     const {onClose, tg} = useTelegram();
 
@@ -29,18 +26,7 @@ export default function App() {
                     </Routes>
                     <Footer/>
                 </div>
-
-                {/*<Button callback={onClose} buttonTitle="Закрыть"/>
-                <Form method='post' onSubmit={(e) => {
-                    e.preventDefault();
-                    const formData = new FormData(e.currentTarget);
-                    console.log(formData.get('search'));
-                }} inputs={[
-                    <Input key={'search'} type={'text'} name={'search'}/>,
-                    <Button key={'submit'} type={'button'} buttonTitle="Отправить"/>,
-                ]}/>*/}
             </main>
         </>
-
     );
 }
