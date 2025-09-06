eb0c0c9138decd2f22c5b554136a8f862c0bca5c
sherbakov andrei <asherbakov@nopreset.ru>
2025-09-06 14:01:40 +0300

    main

--- src/components/Buttons/Button.js
+++
@@ -1,6 +0,0 @@
-import "./Button.module.css";
-export default function Button({buttonTitle, callback, type}) {
-    return (
-        <button className='button' onClick={callback} type={type}>{buttonTitle}</button>
-    );
-}
