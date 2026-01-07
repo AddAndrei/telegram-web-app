cd03c00cbc8b869dd625d3cbdab50f0f7fa1308a
sherbakov andrei <asherbakov@nopreset.ru>
2026-01-07 15:25:49 +0300

    main

--- dist/telephone-tg-app/server/main.server.mjs
+++ dist/telephone-tg-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BFQL4D6U.mjs";import"./chunk-VOQYUJZS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-JHXUDEXS.mjs";import"./chunk-XJBPVS4J.mjs";import"./chunk-U3XBCN5O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/services/profile/profile.service.ts
+++ src/app/services/profile/profile.service.ts
@@ -8,11 +8,6 @@ import {HttpHeaders} from "@angular/common/http";
 export class ProfileService extends HttpService {
 
   getProfile() {
-    let tok: string | null = localStorage.getItem('token');
-    const headers = new HttpHeaders().set('Authorization', '' + tok)
-    console.log(localStorage.getItem('token'));
-    return this.http.get(this.url + "profile", {
-      headers
-    });
+    return this.http.get(this.url + "profile");
   }
 }
