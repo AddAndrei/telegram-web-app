96b2ac42de75d8aff1c9964dc08f7f95d8f361b1
sherbakov andrei <asherbakov@nopreset.ru>
2026-01-07 15:28:03 +0300

    main

--- dist/telephone-tg-app/server/main.server.mjs
+++ dist/telephone-tg-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BFQL4D6U.mjs";import"./chunk-VOQYUJZS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CGZMPMDC.mjs";import"./chunk-HIQJWNHZ.mjs";import"./chunk-MAERUXNA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
